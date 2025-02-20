0d215b6f9b2a5d59487b5ef85531a7283b3736e8
Almohsen <50508250+ALLMOHSEN@users.noreply.github.com>
2025-02-20 04:50:52 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = "mongodb+srv://almohsen:qh9aomBWnSuNNpHT@cluster0.mongodb.net/warehouseDB?retryWrites=true&w=majority";
 // إعدادات السيرفر
 app.use(express.json());
